87e6b27f30d6bc3ff3c9d978f735fcc342a33bbc
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-07-23 14:33:07 +0400

    Refactor: extract test function to build Mars Rover (apply to the rest)

--- src/MarsRover.test.ts
+++ src/MarsRover.test.ts
@@ -19,8 +19,8 @@ describe.each([
   {input: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'N'}},
 ])('Mars Rover', ({input, expected}) => {
   test(`stays at the landing position: ${input.x}, ${input.y} facing ${input.direction}`, () => {
-    const startingPositionRover = new MarsRover(Position.at(input.x, input.y).facing(input.direction))
-    const expectedPositionRover = new MarsRover(Position.at(expected.x, expected.y).facing(expected.direction))
+    const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+    const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, expected.direction)
 
     // no movement
 
@@ -33,8 +33,8 @@ describe.each([
   {input: {x: 0, y: 0, direction: 'N'}, commands: 'b', expected: {x: 0, y: -1, direction: 'N'}}
 ])('Mars Rover', ({input, commands, expected}) => {
   test(`moves after receiving one command: ${input.x}, ${input.y} facing ${input.direction}`, () => {
-    const startingPositionRover = new MarsRover(Position.at(input.x, input.y).facing(input.direction))
-    const expectedPositionRover = new MarsRover(Position.at(expected.x, expected.y).facing(expected.direction))
+    const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
+    const expectedPositionRover = aMarsRoverAt(expected.x, expected.y, expected.direction)
 
     startingPositionRover.move(commands)
 
