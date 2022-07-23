06c7f3acf66940a63b06d2a727e5a92f4d2096f8
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-07-23 14:05:42 +0400

    Green

--- src/MarsRover.test.ts
+++ src/MarsRover.test.ts
@@ -1,29 +1,15 @@
 import { assert, property, string } from 'fast-check'
-import { MarsRover } from './MarsRover'
-
-describe('Hiker', () => {
-  it('answers 42 for any question', () => {
-    assert(property(string(), (question) => {
-      // Arrange
-      const hiker = new MarsRover()
-
-      // Act
-      const answer = hiker.askQuestion(question)
-
-      // Assert
-      expect(answer).toBe(42)
-    }))
-  })
-})
+import { MarsRover, Position } from './MarsRover'
 
 describe.each([
-  {input: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'N'}}
+  {input: {x: 0, y: 0, direction: 'N'}, expected: {x: 0, y: 0, direction: 'N'}},
 ])('Mars Rover', ({input, expected}) => {
   test(`stays at the landing position: ${input.x}, ${input.y} facing ${input.direction}`, () => {
     const startingPositionRover = new MarsRover(Position.at(input.x, input.y).facing(input.direction))
+    const expectedPositionRover = new MarsRover(Position.at(expected.x, expected.y).facing(expected.direction))
 
     // no movement
 
-    const expectedPositionRover = new MarsRover(Position.at(expected.x, expected.y).facing(expected.direction))
+    expect(startingPositionRover).toEqual(expectedPositionRover)
   })
 })
