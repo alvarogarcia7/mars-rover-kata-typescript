d01b958ff1811bd08b1556f1e035f538d9fc64ec
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-07-23 14:32:16 +0400

    Refactor: extract test function to build Mars Rover

--- src/MarsRover.test.ts
+++ src/MarsRover.test.ts
@@ -1,9 +1,13 @@
 import {MarsRover, Position} from './MarsRover'
 
+function aMarsRoverAt(x, y, directionRaw) {
+  return new MarsRover(Position.at(x, y).facing(directionRaw))
+}
+
 describe('Mars Rover', () => {
   test(`cannot accept an invalid command`, () => {
     const input = {x: 0, y: 0, direction: 'N'}
-    const startingPositionRover = new MarsRover(Position.at(input.x, input.y).facing(input.direction))
+    const startingPositionRover = aMarsRoverAt(input.x, input.y, input.direction)
 
     expect(() => {
       startingPositionRover.move('X')
